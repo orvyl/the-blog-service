eb48aff3bf7f9ddd87e2b2a58f1537ba17cb03c7
orvyl <orvyl.t@gmail.com>
2023-07-11 10:51:24 +0800

    add timestamp approach

--- src/main/java/com/apper/theblogservice/model/Blogger.java
+++ src/main/java/com/apper/theblogservice/model/Blogger.java
@@ -5,8 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "BLOGGER")
@@ -26,5 +29,13 @@ public class Blogger {
 
     @Column(name = "PASSWORD")
     private String password;
+
+    @Column(name = "CREATED_AT")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void setCreatedAt() {
+        createdAt = LocalDateTime.now();
+    }
 
 }
