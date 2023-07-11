de94e0cde6a5d124b85c5600ea6193b553b4c5ac
orvyl <orvyl.t@gmail.com>
2023-07-11 10:45:50 +0800

    add auto generation of id

--- src/main/java/com/apper/theblogservice/model/Blogger.java
+++ src/main/java/com/apper/theblogservice/model/Blogger.java
@@ -2,6 +2,8 @@ package com.apper.theblogservice.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -13,6 +15,7 @@ public class Blogger {
 
     @Id
     @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
     @Column(name = "COMPLETE_NAME")
