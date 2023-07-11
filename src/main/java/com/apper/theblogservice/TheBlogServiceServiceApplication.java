593e2c8226b8c487f2e1eb2e93ecf7bea44e12d3
orvyl <orvyl.t@gmail.com>
2023-07-11 09:40:50 +0800

    batman commit

---
+++ src/main/java/com/apper/theblogservice/TheBlogServiceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.apper.theblogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBlogServiceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBlogServiceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apper/theblogservice/TheBlogServiceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apper.theblogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheBlogServiceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
